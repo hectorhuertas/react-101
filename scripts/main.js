4bb6acca0ecbcfebb9254bdc4c8b8f6ee0a9d1df
Hector Huertas <hectorhuertas@gmail.com>
2016-06-10 00:11:55 +0200

    Implement adding new fish

--- scripts/main.js
+++ scripts/main.js
@@ -11,7 +11,17 @@ var createBrowserHistory = require('history/lib/createBrowserHistory');
 var h = require('./helpers');
 
 var App = React.createClass({
-
+  getInitialState: function() {
+    return ({
+      fishes: {},
+      order: {}
+    });
+  },
+  addFish: function(fish) {
+    var timestamp = (new Date()).getTime();
+    this.state.fishes['fish-' + timestamp] = fish;
+    this.setState({ fishes: this.state.fishes });
+  },
   render: function() {
     return (
       <div className='catch-of-the-day'>
@@ -19,7 +29,7 @@ var App = React.createClass({
           <Header tagline='Fresh Seafood Market' />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish}/>
       </div>
     );
   }
@@ -48,7 +58,10 @@ var Inventory = React.createClass({
 
   render: function() {
     return (
-      <p>Inventory</p>
+      <div>
+        <h2>Inventory</h2>
+        <AddFishForm {...this.props}/>
+      </div>
     );
   }
 
@@ -83,6 +96,37 @@ var StorePicker = React.createClass({
     );
   }
 
+});
+
+var AddFishForm = React.createClass({
+  createFish: function(e) {
+    e.preventDefault();
+    var fish = {
+      name: this.refs.name.value,
+      price: this.refs.price.value,
+      status: this.refs.status.value,
+      desc: this.refs.desc.value,
+      image: this.refs.image.value
+    };
+    this.props.addFish(fish);
+    this.refs.fishForm.reset();
+  },
+
+  render: function() {
+    return (
+      <form className="fish-edit" ref="fishForm" onSubmit={this.createFish}>
+        <input type="text" ref="name" placeholder="Fish Name"/>
+        <input type="text" ref="price" placeholder="Fish Price" />
+        <select ref="status">
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold Out!</option>
+        </select>
+        <textarea type="text" ref="desc" placeholder="Desc"></textarea>
+        <input type="text" ref="image" placeholder="URL to Image" />
+        <button type="submit">+ Add Item </button>
+      </form>
+    );
+  }
 });
 
 var NotFound = React.createClass({
