d557dec8a267a43d5d13b3f77c23e415d5cf24a4
Hector Huertas <hectorhuertas@gmail.com>
2016-06-09 23:37:14 +0200

    Implement navigate to store

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,7 @@ var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var Navigation = ReactRouter.Navigation;
+var History = ReactRouter.History;
 
 var createBrowserHistory = require('history/lib/createBrowserHistory');
 var h = require('./helpers');
@@ -64,10 +65,17 @@ var Order = React.createClass({
 });
 
 var StorePicker = React.createClass({
-
+  mixins: [History],
+  goToStore: function(e) {
+    e.preventDefault();
+    // get the data from the input
+    var storeId = this.refs.storeId.value;
+    // transition from storepicker to app
+    this.history.pushState(null, '/store/' + storeId);
+  },
   render: function() {
     return (
-      <form className='store-selector'>
+      <form className='store-selector' onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
         <input type='text' ref='storeId' defaultValue={h.getFunName()}/>
         <input type='submit'/>
